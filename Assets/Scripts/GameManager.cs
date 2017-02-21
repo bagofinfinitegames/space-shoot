ec28c10b58df4623ee090890417c3d29917e50ec
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-02-20 21:29:44 -0700

    basic project setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        getSaveInfo();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //bool set = false;
+        //if (!set) {
+        //    setSaveInfo(5);
+        //    set = true;
+        //}
+	}
+
+    void getSaveInfo() {
+        int maxScore = PlayerPrefs.GetInt("maxScore", 0);
+        Debug.Log(maxScore);
+    }
+
+    void setSaveInfo(int maxScore) {
+        PlayerPrefs.SetInt("maxScore", maxScore);
+        getSaveInfo();
+    }
+}
